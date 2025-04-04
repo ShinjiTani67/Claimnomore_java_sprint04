25efb9fa3dd242c203755f9071802aeb4704dd48
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-04 09:38:59 -0300

    create claim.java e claimdto

---
+++ src/main/java/dto/ClaimDTO.java
@@ -0,0 +1,24 @@
+package dto;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ClaimDTO {
+
+    private long id;
+
+    private ZonedDateTime claimDate;
+    
+    private String description;
+}
