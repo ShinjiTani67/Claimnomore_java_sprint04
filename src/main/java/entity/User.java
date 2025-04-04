69e6eeac1fd0ed5272df2e89848a41884aa07006
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-04 09:27:15 -0300

    create user.java

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,35 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Data
+@ToString
+@Entity
+@Table(name="USUARIO")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "email_user", length = 100, nullable = false)
+    private String email;
+
+    @Column(name = "name_user", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "cpf", length = 100, nullable = false)
+    private int CPF;
+
+    @DateTimeFormat
+    @Column(name = "admission_date", length = 100, nullable = false)
+    private int admissiondate;
+
+    @Column(name = "job", length = 100, nullable = false)
+    private String job;
+
+}
