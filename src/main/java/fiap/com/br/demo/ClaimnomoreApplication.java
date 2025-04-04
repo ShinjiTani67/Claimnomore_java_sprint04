4729920982488e4f463c74cfb6f94e4612f9227d
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-04 09:15:33 -0300

    innit git

---
+++ src/main/java/fiap/com/br/demo/ClaimnomoreApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.br.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaimnomoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaimnomoreApplication.class, args);
+	}
+
+}
