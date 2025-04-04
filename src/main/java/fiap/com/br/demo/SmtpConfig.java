fd340e2db5167255ad66f04c5c2c54779cc8aee9
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-04 09:47:36 -0300

    update smtpconfig

--- src/main/java/fiap/com/br/demo/SmtpConfig.java
+++ src/main/java/fiap/com/br/demo/SmtpConfig.java
@@ -1,23 +1,36 @@
 package fiap.com.br.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
-public class SmtpConfig {
+public class StmpConfig {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    //@Value("${spring.mail.properties.mail.stmp.auth}")
+    //private Boolean auth;
 
     @Bean
-    public JavaMailSender javaMailSender() {
+    public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("seu.email@gmail.com");
-        mailSender.setPassword("sua_senha");
+
+        mailSender.setHost(host);
+        mailSender.setPort(port);
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.smtp.auth", true);
-        props.put("mail.smtp.starttls.enable", true);
+        props.put("mail.smtp.auth", false);
 
         return mailSender;
+
     }
 }
