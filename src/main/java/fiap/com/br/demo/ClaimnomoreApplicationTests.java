45667262cea3b196e71c8f9a38a003f12f864ecf
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-08 11:32:46 -0300

    update geral

--- src/main/java/fiap/com/br/demo/ClaimnomoreApplicationTests.java
+++ src/main/java/fiap/com/br/demo/ClaimnomoreApplicationTests.java
@@ -1,11 +1,13 @@
 package fiap.com.br.demo;
 
-
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClaimnomoreApplicationTests {
+
     @Test
     void contextLoads() {
+
     }
 }
-
