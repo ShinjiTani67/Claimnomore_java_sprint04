10ef82b6d80a597dd6a06ac03c0ec47ec1c22d98
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-08 11:26:51 -0300

    update pom.xml e ApplicationTest

--- src/main/java/fiap/com/br/demo/ClaimnomoreApplicationTests.java
+++ src/main/java/fiap/com/br/demo/ClaimnomoreApplicationTests.java
@@ -1,7 +1,6 @@
 package fiap.com.br.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class ClaimnomoreApplicationTests {
