9295c2c0528643cd577218621ac04e97d5df8279
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-28 09:00:14 -0300

    update enterprise repository

--- src/main/java/fiap/com/br/demo/repository/EnterpriseRepository.java
+++ src/main/java/fiap/com/br/demo/repository/EnterpriseRepository.java
@@ -2,8 +2,11 @@ package fiap.com.br.demo.repository;
 
 import fiap.com.br.demo.entity.Enterprise;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EnterpriseRepository extends MongoRepository<Enterprise, UUID> {
+
+    Optional<Enterprise> findByUuid(UUID uuid);
+
 }
