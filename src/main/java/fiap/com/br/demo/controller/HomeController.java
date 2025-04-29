78650b7934ac3d943e096f6306f287d880e88056
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-04-29 10:35:06 -0300

    create home controller

---
+++ src/main/java/fiap/com/br/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package fiap.com.br.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "/";
+    }
+}
