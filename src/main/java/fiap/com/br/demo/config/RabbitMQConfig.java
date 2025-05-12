7e78c5303ea88787ae66ce2efdbe75e13c90fda1
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2025-05-12 14:21:15 -0300

    create rabbitmqconfig

---
+++ src/main/java/fiap/com/br/demo/config/RabbitMQConfig.java
@@ -0,0 +1,18 @@
+package fiap.com.br.demo.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Queue;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "mensagens";
+
+    @Bean
+    public Queue queue() {
+        return new Queue<>(QUEUE_NAME, true);
+    }
+}
